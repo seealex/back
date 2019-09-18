f22d98a8139ff82d7a08b9133ff7df3f78c86423
Alex Silva <asilva3@uolinc.com>
2019-09-18 00:18:19 -0300

    v2

--- src/main/java/br/com/alex/twitter/controller/IntegrationController.java
+++ src/main/java/br/com/alex/twitter/controller/IntegrationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/totals")
+@RequestMapping("/api")
 @Log4j2
 public class IntegrationController {
 
